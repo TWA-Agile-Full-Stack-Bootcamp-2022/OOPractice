1f0d5a8243714c93a10cc26b80e8f964d84445f7
Guangguang Ma <ggma@thoughtworks.com>
2022-10-12 13:18:58 +0800

    feat: create class Driver and show message of car speeding up when driver drive a car

---
+++ OOPracticeTest/DriverTest.cs
@@ -0,0 +1,18 @@
+using OOPractice;
+using Xunit;
+
+namespace OOPracticeTest
+{
+    public class DriverTest
+    {
+        [Fact]
+        public void Should_show_speed_up_message_when_driver_drive_a_car()
+        {
+            IVehicle car = new Car();
+
+            var message = Driver.Drive(car);
+
+            Assert.Equal("Cool Car: speed up 30 km/h", message);
+        }
+    }
+}
