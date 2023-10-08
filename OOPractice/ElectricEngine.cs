cd2d78b753a6144a2544e75385ee2b16d74a4dd6
Hong Wu <hong.wu@rea-group.com>
2023-10-08 16:55:23 +0800

    feat: create Car and Engine classes

---
+++ OOPractice/ElectricEngine.cs
@@ -0,0 +1,10 @@
+namespace OOPractice
+{
+    public class ElectricEngine : IEngine
+    {
+        public decimal GetSpeed()
+        {
+            return 25;
+        }
+    }
+}
---
+++ OOPractice/IEngine.cs
@@ -0,0 +1,7 @@
+namespace OOPractice
+{
+    public interface IEngine
+    {
+        decimal GetSpeed();
+    }
+}
