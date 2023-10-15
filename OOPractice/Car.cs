52abd6e87c3f1d7c1d9180c1d47e1a1d1947e602
Jiancheng LI <jiancheng.li@thoughtworks.com>
2023-10-15 08:59:06 +0800

    fix: change constructor order

--- OOPractice/Car.cs
+++ OOPractice/Car.cs
@@ -2,12 +2,12 @@
 {
     public class Car
     {
-        public string Name { get; set; }
-
         public Car(string name)
         {
             Name = name;
         }
+
+        public string Name { get; set; }
 
         public string ShowMessage()
         {
