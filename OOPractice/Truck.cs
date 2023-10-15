486f120597715abf7510e298c73e49b2143b8ec1
Jiancheng LI <jiancheng.li@thoughtworks.com>
2023-10-15 08:53:10 +0800

    feat: should show  truck message

---
+++ OOPractice/Truck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPractice
+{
+    public class Truck
+    {
+        public Truck(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+
+        public string ShowMessage()
+        {
+            return "Big Truck: speed up 10 km/h";
+        }
+    }
+}
---
+++ OOPracticeTest/TruckTest.cs
@@ -0,0 +1,19 @@
+﻿namespace OOPracticeTest
+{
+    using OOPractice;
+    using Xunit;
+
+    public class TruckTest
+    {
+        [Fact]
+        public void Should_show_truck_message()
+        {
+            // given
+            var bigTruck = new Truck("A Truck");
+            // when
+            string message = bigTruck.ShowMessage();
+            // then
+            Assert.Equal("Big Truck: speed up 10 km/h", message);
+        }
+    }
+}
