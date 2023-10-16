f999e19fc996c4edd3c04051cfc7d13c7b327e08
Yixi <lyh1112@gmail.com>
2023-10-16 16:22:17 +0800

    feat: add a Car class

---
+++ OOPractice/Car.cs
@@ -0,0 +1,19 @@
+namespace OOPractice
+{
+    public class Car
+    {
+        private string name;
+        private int speed;
+
+        public Car(string name, int speed)
+        {
+            this.name = name;
+            this.speed = speed;
+        }
+
+        public string Speedup()
+        {
+            return $"{this.name}: speed up {this.speed} km/h";
+        }
+    }
+}
--- OOPracticeTest/UnitTest1.cs
+++ OOPracticeTest/UnitTest1.cs
@@ -11,5 +11,16 @@ namespace OOPracticeTest
             var class1 = new Class1();
             Assert.NotNull(class1);
         }
+
+        [Fact]
+        public void Should_return_name_and_speed_when_speedup_given_car_name_and_speed()
+        {
+            //given
+            var car = new Car("Cool Car", 30);
+            //when
+            var actual = car.Speedup();
+            //then
+            Assert.Equal("Cool Car: speed up 30 km/h", actual);
+        }
     }
 }
